543f86749cd91b906e06a132ad6dab2d682e9838
Kyle Freemantle <kcfreemantle@gmail.com>
2023-03-01 14:08:06 -0800

    challenge 3 complete

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -39,7 +39,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 ------------------------------------------------------------------------------------------------ */
 
 const containsAnd = (arr) => {
-  // Solution code here...
+  return arr.filter( (arrayElement) => arrayElement.includes('and'));
 };
 
 /* ------------------------------------------------------------------------------------------------
