3db4fb6f5cbcde0c18f51b2d94ffceba7cedbf25
Kyle Freemantle <kcfreemantle@gmail.com>
2023-03-03 14:57:36 -0800

    CC1 done w/ help from Michael Dulin

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -12,7 +12,8 @@ Note the space in between first and last names.
 You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
 const toLastNames = people => {
-  // Solution code here...
+  let newArr = people.map((nameElement) => `${nameElement.firstName} ${nameElement.lastName}`);
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
