02c280be53a9fc2da5b6b2e9a1c35dd8ccd79dd4
Kyle Freemantle <kcfreemantle@gmail.com>
2023-03-03 21:16:31 -0800

    completed CC5

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -129,6 +129,7 @@ Write a function named reversedString that takes in a string and returns a strin
 
 Note: You must use reduce for this challenge. You may not use the built-in .reverse() string method.
 ------------------------------------------------------------------------------------------------ */
+arrSheyna = ['T', 'A', 'C'];
 
 const reversedString = (str) => {
   return [...str].reduce((a, b) => b.concat(a));
